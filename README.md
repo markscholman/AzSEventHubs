73ae8be267d05dc1ed7cfbdc628fba36e217b746
Mark Scholman <8298597+markscholman@users.noreply.github.com>
2020-04-02 14:15:21 -0400

    add sample app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# AzSEventHubs
+# Azure Stack Event Hubs
+
+## Introduction
+
+This is the sample code i used for the YouTube video "Azure Stack Hub - Event Hubs (Tenant Experience)
+
+[![Azure Stack Event Hubs](http://img.youtube.com/vi/n4XyMt-kWuI/0.jpg)](http://www.youtube.com/watch?v=n4XyMt-kWuI "Azure Stack Event Hubs")
+
+## How to use
+
+Deploy Azure Stack EventHub on your Azure Stack environment and configure namespace and eventhub. Then create a access policy and use this connection string in this sample Sender and Receiver app.
---
+++ ReceiveDataApp-CSharp/ReceiveDataApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace ReceiveDataApp
+{
+    public class Program
+    {
+        private const string EventHubConnectionString = "";
+        private const string EventHubName = "";
+        private const string StorageContainerName = "";
+
+        private static readonly string StorageConnectionString = "";
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EventHubConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
